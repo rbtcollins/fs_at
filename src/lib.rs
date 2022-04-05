09889e37213fb7886841f5522107989de5df88ac
Robert Collins <robertc@robertcollins.net>
2022-04-05 22:57:58 +1200

    Implement mkdirat
    Includes unix and Windows implementations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,101 @@
+//! Extension for operations that manipulate the file system relative to an open
+//! directory, rather than the global namespace.
+//!
+//! NB: If a missing capability or platform is found, I will happily add /
+//! accept patches : features are being added as needed, rather than
+//! speculatively.
+
+use std::{
+    fs::File,
+    io::{Error, ErrorKind, Result},
+    path::Path,
+};
+
+cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+        mod win;
+
+        use win::OpenOptionsImpl;
+    } else {
+        mod unix;
+
+        use unix::OpenOptionsImpl;
+    }
+}
+
+/// Similar to [`std::fs::OpenOptions`], this struct is used to parameterise the
+/// various at functions, which are then called on the struct itself. Typical
+/// use is to create a struct via [`Default::default`] or
+/// [`OpenOptions::default()`], and then customise it as desired (e.g. setting
+/// security descriptors on windows, or mode on unix) using an appropriate
+/// platform specific trait, finishing up with the desired manipulation e.g.
+/// `mkdirat`.
+#[derive(Default, Debug)]
+pub struct OpenOptions {
+    _impl: OpenOptionsImpl,
+}
+
+impl OpenOptions {
+    /// Create a directory relative to an open directory. Errors if a rooted
+    /// path is provided.
+    ///
+    /// Returns a [`File`] opened on the created directory.
+    ///
+    /// On Windows this is done without resolving names a second time, in a
+    /// single syscall.
+    ///
+    /// On Unix, an additional openat syscall is performed to open the created
+    /// directory. This limitation may be lifted in future if the mooted
+    /// mkdirat2 call gets created.. The mode of the new directory defaults to
+    /// 0o777.
+    pub fn mkdirat<P: AsRef<Path>>(&self, f: &mut File, p: P) -> Result<File> {
+        let path = p.as_ref();
+        if path.has_root() {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Rooted file path {path:?}"),
+            ));
+        }
+        self._impl.mkdirat(f, path)
+    }
+}
+
+pub mod os {
+    cfg_if::cfg_if! {
+        if #[cfg(windows)] {
+            pub use crate::win::exports as windows;
+        } else {
+            pub use crate::unix::exports as unix;
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod testsupport;
+
 #[cfg(test)]
 mod tests {
+    use std::{
+        fs::{rename, File},
+        io::Result,
+    };
+
+    use tempfile::TempDir;
+
+    use crate::{testsupport::open_dir, OpenOptions};
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn mkdirat() -> Result<()> {
+        let tmp = TempDir::new()?;
+        let parent = tmp.path().join("parent");
+        let renamed_parent = tmp.path().join("renamed-parent");
+        std::fs::create_dir(&parent)?;
+        let mut parent_file = open_dir(&parent)?;
+        rename(parent, &renamed_parent)?;
+        let _child: File = OpenOptions::default().mkdirat(&mut parent_file, "child")?;
+        let expected = renamed_parent.join("child");
+        let metadata = expected.symlink_metadata()?;
+        assert!(metadata.is_dir());
+        Ok(())
     }
 }
