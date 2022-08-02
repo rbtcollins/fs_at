7be9aa252687c63cea0085164d3f8739fb93b9fe
Robert Collins <robertc@robertcollins.net>
2022-08-03 02:50:14 +1200

    Implement basic openat

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@
 //! NB: If a missing capability or platform is found, I will happily add /
 //! accept patches : features are being added as needed, rather than
 //! speculatively.
+//!
+//! The Rust standard library does not (yet) offer at filesystem calls as a core
+//! feature. For instance `mkdirat`. These calls are essential for writing
+//! race-free filesystem code, since otherwise the state of the filesystem path
+//! that operations are executed against can change silently, leading to TOC-TOU
+//! race conditions. For Unix these calls are readily available in the libc
+//! crate, but for Windows some more plumbing is needed. This crate provides a
+//! unified Rust-y interface to these calls.
 
 use std::{
     fs::File,
@@ -36,6 +44,13 @@ pub struct OpenOptions {
 }
 
 impl OpenOptions {
+    /// Set the option to create a new file when missing, while still opening
+    /// existing files.
+    pub fn create(&mut self, create: bool) -> &mut Self {
+        self._impl.create(create);
+        self
+    }
+
     /// Create a directory relative to an open directory. Errors if a rooted
     /// path is provided.
     ///
@@ -48,15 +63,28 @@ impl OpenOptions {
     /// directory. This limitation may be lifted in future if the mooted
     /// mkdirat2 call gets created.. The mode of the new directory defaults to
     /// 0o777.
-    pub fn mkdirat<P: AsRef<Path>>(&self, f: &mut File, p: P) -> Result<File> {
-        let path = p.as_ref();
-        if path.has_root() {
+    pub fn mkdir_at<P: AsRef<Path>>(&self, d: &mut File, p: P) -> Result<File> {
+        self._impl
+            .mkdir_at(d, OpenOptions::ensure_root(p.as_ref())?)
+    }
+
+    /// Opens a file at the path p relative to the directory d.
+    ///
+    /// This will honour the options set for creation/append etc, but will only
+    /// operate relative to d. To open a file with an absolute path, use the
+    /// stdlib fs::OpenOptions.
+    pub fn open_at<P: AsRef<Path>>(&self, d: &mut File, p: P) -> Result<File> {
+        self._impl.open_at(d, OpenOptions::ensure_root(p.as_ref())?)
+    }
+
+    fn ensure_root(p: &Path) -> Result<&Path> {
+        if p.has_root() {
             return Err(Error::new(
                 ErrorKind::Other,
-                format!("Rooted file path {path:?}"),
+                format!("Rooted file path {p:?}"),
             ));
         }
-        self._impl.mkdirat(f, path)
+        Ok(p)
     }
 }
 
@@ -78,24 +106,70 @@ mod tests {
     use std::{
         fs::{rename, File},
         io::Result,
+        path::PathBuf,
     };
 
     use tempfile::TempDir;
 
     use crate::{testsupport::open_dir, OpenOptions};
 
-    #[test]
-    fn mkdirat() -> Result<()> {
+    /// Create a directory parent, open it, then rename it to renamed-parent and
+    /// create another directory in its place. returns the file handle and the
+    /// final path.
+    fn setup() -> Result<(TempDir, File, PathBuf)> {
         let tmp = TempDir::new()?;
         let parent = tmp.path().join("parent");
         let renamed_parent = tmp.path().join("renamed-parent");
         std::fs::create_dir(&parent)?;
-        let mut parent_file = open_dir(&parent)?;
+        let parent_file = open_dir(&parent)?;
         rename(parent, &renamed_parent)?;
-        let _child: File = OpenOptions::default().mkdirat(&mut parent_file, "child")?;
+        Ok((tmp, parent_file, renamed_parent))
+    }
+
+    #[test]
+    fn mkdirat_simple() -> Result<()> {
+        // mk a new dir
+        let (_tmp, mut parent_file, renamed_parent) = setup()?;
+        let _child: File = OpenOptions::default().mkdir_at(&mut parent_file, "child")?;
         let expected = renamed_parent.join("child");
         let metadata = expected.symlink_metadata()?;
         assert!(metadata.is_dir());
+        Ok(())
+    }
+
+    #[test]
+    fn mkdirat_permit_existing() -> Result<()> {
+        let (_tmp, mut parent_file, renamed_parent) = setup()?;
+        OpenOptions::default().mkdir_at(&mut parent_file, "child")?;
+        let _child: File = OpenOptions::default()
+            .create(true)
+            .mkdir_at(&mut parent_file, "child")?;
+        let expected = renamed_parent.join("child");
+        let metadata = expected.symlink_metadata()?;
+        assert!(metadata.is_dir());
+        Ok(())
+    }
+
+    #[test]
+    fn mkdirat_error_existing() -> Result<()> {
+        let (_tmp, mut parent_file, _) = setup()?;
+        OpenOptions::default().mkdir_at(&mut parent_file, "child")?;
+        OpenOptions::default()
+            .mkdir_at(&mut parent_file, "child")
+            .unwrap_err();
+        Ok(())
+    }
+
+    #[test]
+    fn openat_create_new() -> Result<()> {
+        let (_tmp, mut parent_file, renamed_parent) = setup()?;
+        let _child: File = OpenOptions::default()
+            .create(true)
+            .open_at(&mut parent_file, "child")?;
+        let expected = renamed_parent.join("child");
+        let metadata = expected.symlink_metadata()?;
+        assert!(metadata.is_file());
+        assert_eq!(metadata.len(), 0);
         Ok(())
     }
 }
