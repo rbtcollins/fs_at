8b00130b5cb3126dd4ac96a3e8f91e386e6ec1fb
pin <voidpin@protonmail.com>
2023-04-10 22:33:11 +0200

    Add NetBSD exception

--- src/lib.rs
+++ src/lib.rs
@@ -323,7 +323,7 @@ impl OpenOptions {
     /// restricted.
     ///
     /// MacOSX: Not implemented as O_PATH is not defined.
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(all(not(target_os = "macos"), not(target_os = "netbsd")))]
     pub fn open_path_at<P: AsRef<Path>>(&self, d: &File, p: P) -> Result<File> {
         self._impl
             .open_path_at(d, OpenOptions::ensure_rootless(p.as_ref())?)
@@ -519,7 +519,7 @@ pub mod testsupport;
 
 #[cfg(test)]
 mod tests {
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(all(not(target_os = "macos"), not(target_os = "netbsd")))]
     use std::path::Path;
     use std::{
         ffi::OsStr,
@@ -1191,7 +1191,7 @@ mod tests {
         Ok(())
     }
 
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(all(not(target_os = "macos"), not(target_os = "netbsd")))]
     #[test]
     fn open_path_at() -> Result<()> {
         let (_tmp, parent_dir, _pathname) = setup()?;
--- src/unix.rs
+++ src/unix.rs
@@ -124,7 +124,7 @@ impl OpenOptionsImpl {
         self._open_at(d, path, flags)
     }
 
-    #[cfg(not(target_os = "macos"))]
+    #[cfg(all(not(target_os = "macos"), not(target_os = "netbsd")))]
     pub fn open_path_at(&self, d: &File, path: &Path) -> Result<File> {
         let flags =
             libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_PATH | libc::O_CLOEXEC | libc::O_NOCTTY;
